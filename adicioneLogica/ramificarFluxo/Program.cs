1c6db82b6ec95374bd76e48f89a5d7e2aa8a84a2
Amanda Shichinoe <amanda.ncosta@hotmail.com>
2021-05-05 19:31:04 -0300

    Switch Case

---
+++ adicioneLogica/ramificarFluxo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ramificarFluxo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SKU = Stock Keeping Unit
+            string sku = "01-MN-L";
+
+            string[] product = sku.Split('-');
+
+            string type = "";
+            string color = "";
+            string size = "";
+            
+            
+            switch(product[0]){
+                case "01":
+                    type = "Sweat shirt";
+                    break;
+                case "02":
+                    type = "T-Shirt";
+                    break;
+
+                case "03":
+                    type = "Sweat pants";
+                    break;
+
+                default:
+                    type = "Other";
+                    break;
+            }
+
+            switch (product[1]){
+                case "BL":
+                    color = "Black";
+                    break;
+
+                case "MN":
+                    color = "Maroon";
+                    break;
+
+                default:
+                    color = "White";
+                    break;
+            }
+
+            switch (product[2]){
+                case "S":
+                    size = "Small";
+                    break;
+
+                case "M":
+                    size = "Medium";
+                    break;
+
+                case "L":
+                    size = "Large";
+                    break;
+
+                default:
+                    size = "One Size Fits All";
+                    break;
+            }
+ 
+            
+
+            Console.WriteLine($"Product: {size} {color} {type}");
+        }
+    }
+}
--- criarAplicativos/README.md
+++ criarAplicativos/README.md
@@ -1,1 +1,9 @@
 # Roteiro de Aprendizado - Criar aplicativos .NET com C#
+
+Este repositório contém os códigos aprendidos e desenvolvidos nos módulos deste roteiro de aprendizado.
+
+
+
+
+
+Roteiro disponível em : https://docs.microsoft.com/pt-br/learn/paths/build-dotnet-applications-csharp/
